f3daa9f8e9fed104400dca69f8313e2c44a7fe00
Mikołaj <153921909+noyex@users.noreply.github.com>
2024-05-07 16:14:18 +0200

    Create zadanie4.php

---
+++ LAB_7/zadanie4.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Imię: <input type="text"><br>
+    Nazwisko: <input type="text"><br>
+    Adres email: <input type="text"><br>
+    Hasło: <input type="text"><br>
+    Potwierdź hasło: <input type="text"><br>
+    Wiek: <input type="text"><br>
+    <button>Zarejestruj się</button>
+</body>
+</html>
