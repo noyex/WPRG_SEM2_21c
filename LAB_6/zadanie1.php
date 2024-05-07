ead1b7825511799cf62bb01b70fecef1a04a2245
Mikołaj <153921909+noyex@users.noreply.github.com>
2024-05-07 16:07:20 +0200

    Create zadanie1.php

---
+++ LAB_6/zadanie1.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $stolice=array("Italy"=>"Rome",
+    "Luxembourg"=>"Luxembourg",
+    "Belgium"=> "Brussels",
+    "Denmark"=>"Copenhagen",
+    "Finland"=>"Helsinki",
+    "France" => "Paris",
+    "Slovakia"=>"Bratislava",
+    "Slovenia"=>"Ljubljana",
+    "Germany" => "Berlin",
+    "Greece" => "Athens",
+    "Ireland"=>"Dublin",
+    "Netherlands"=>"Amsterdam",
+    "Portugal"=>"Lisbon",
+    "Spain"=>"Madrid",
+    "Sweden"=>"Stockholm",
+    "United Kingdom"=>"London",
+    "Cyprus"=>"Nicosia",
+    "Lithuania"=>"Vilnius",
+    "Czech Republic"=>"Prague",
+    "Estonia"=>"Tallin",
+    "Hungary"=>"Budapest",
+    "Latvia"=>"Riga","Malta"=>"Valetta",
+    "Austria" => "Vienna", 
+    "Poland"=>"Warsaw");
+    asort($stolice);
+    foreach(array_keys($stolice) as $key)
+    {
+        echo "The capital of ",$key," is ", $stolice[$key],"<br>";
+    }
+    ?>
+</body>
+</html>
